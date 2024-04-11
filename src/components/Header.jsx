3342d550be6c94abaf877cff3355269dcfbdd4f0
Pankajsiwach123321 <pankusiwach000@gmai.com>
2024-04-11 12:36:39 +0530

    change imgae

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Mainheader from "./Mainheader";
-import dashBoardImg from "../assets/images/dashboardimage.webp";
+import dashBoardImg from "../assets/images/dashboard.webp";
 import { HeroGradiant } from "./comman/Icon";
 
 const Header = () => {
@@ -17,7 +17,7 @@ const Header = () => {
       <img
         src={dashBoardImg}
         alt="dashBoardImg"
-        className="mx-auto lg:-translate-y-[43%] -translate-y-[55%]  pl-4 sm:pl-5 md:pl-8 lg:pl-12 lg:w-[915px] md:w-[600px] max-h-[400px] md:max-h-[687px] relative z-10"
+        className="mx-auto lg:-translate-y-[43%] -translate-y-[55%] rounded-md  pl-4 sm:pl-5 md:pl-8 lg:pl-12 lg:w-[915px] shadow-[40.65px_-0.99px_8.92px_0_#0003] md:w-[600px] max-h-[400px] md:max-h-[687px] xl:h-[687px] relative z-10"
       />
     </>
   );
